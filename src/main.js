4befe84ff158aba00fa8b6a18e9df023e834cafc
Jason M <jbird206@gmail.com>
2020-02-07 13:32:44 -0800

    Add files via upload
    add an html form, and some logic for planet age calculation. Also added test folder

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import $ from 'jQuery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+/////user interface///
+$(document).ready(function() {
+function planets (form) {
+    form.marsyears.value = form.humanyears.value / .241
+}
+    form.plutoyears.value = form.humanyears.value/ 248.6
+    
+})
---
+++ src/planets.js
@@ -0,0 +1,22 @@
+export class planets {
+    constructor(form) {
+        this.form = age;
+    
+}
+
+mercuryAge() {
+this.form = form.marsyears.value = form.humanyears.value / .241;
+}
+
+plutonianAge() {
+    this.form = form.plutoyears.value = form.humanyears.value / 248.6;
+}
+
+marsAge() {
+    this.form = form.marsyears.value = form.humanyears.value / 1.88;
+}
+
+venusAge() {
+    this.form = form.venusyears.value = form.humanyears.value / .62;
+}
+}
